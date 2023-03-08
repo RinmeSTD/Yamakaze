6a031b4b9b4c94963ee5a22ad962cc6d7c64a45f
Rinme <rinmesk@yahoo.com>
2023-03-08 18:40:46 +0700

    UT | Update to djsv14 and test passed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { Constants, Intents, Util } = require('discord.js');
+const { Constants, GatewayIntentBits, mergeDefault } = require('discord.js');
 const { Indomitable } = require('indomitable');
 const { token } = require('./config.json');
-const { GUILDS, GUILD_MEMBERS, GUILD_BANS, GUILD_VOICE_STATES, GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS } = Intents.FLAGS;
+const { GUILDS, GUILD_MEMBERS, GUILD_BANS, GUILD_VOICE_STATES, GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS } = GatewayIntentBits;
 const YamakazeClient = require('./src/Yamakaze.js');
 
 // cache settings on client file
@@ -12,7 +12,7 @@ const customClientOptions = {
 };
 
 const sharderOptions = {
-    clientOptions: Util.mergeDefault(Constants.DefaultOptions, customClientOptions),
+    clientOptions: mergeDefault(Constants.DefaultOptions, customClientOptions),
     clusterCount: 4,
     shardCount: 8,
     client: YamakazeClient,
