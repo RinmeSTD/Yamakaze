b1d6d3724a0bce06d228ce919a918f90ec07283b
りんめ <rinmesk@yahoo.com>
2024-04-16 02:38:27 +0700

    chore: tiktok download command done

--- src/interactions/qol/Tiktok.ts
+++ src/interactions/qol/Tiktok.ts
@@ -1,20 +1,22 @@
 import { RESTPostAPIChatInputApplicationCommandsJSONBody } from "discord-api-types/v10";
-import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { CommandOptions, Interaction } from "../../structure/Interaction.js";
 import { Yamakaze } from "../../Yamakaze.js";
 import { InteractionContext } from "../../structure/InteractionContext.js";
+import Tiktok from "@tobyg74/tiktok-api-dl";
 
 export const CommandData = new SlashCommandBuilder()
     .setName("tiktok")
     .setDescription("Download the video from tiktok")
-    .addStringOption(option =>
+    .addStringOption((option) =>
         option
-            .setName('URL')
+            .setName('url')
             .setDescription('The url of tiktok video')
+            .setRequired(true)
     )
     .toJSON();
 
-export default class Tiktok extends Interaction {
+export default class Tiktokd extends Interaction {
     public readonly commandData: RESTPostAPIChatInputApplicationCommandsJSONBody;
     public readonly commandOptions: CommandOptions;
     constructor(client: Yamakaze, directory: string) {
@@ -23,28 +25,44 @@ export default class Tiktok extends Interaction {
         this.commandOptions = {};
     }
 
+
     async run(context: InteractionContext): Promise<void> {
         // Target
-        let url = context.interaction.options.getString("URL")!;
+        let url = context.interaction.options.getString("url")!;
 
         const ping = Math.round(context.interaction.guild!.shard.ping);
+        const result = await Tiktok.Downloader(url, { version: "v2" });
+        // Check if result is defined and has the expected structure
+        if (result && result.result && result.result.video && result.result.music && result.result.author && result.result.author.avatar && result.result.desc) {
+            // Extract data from the result
+            const { video, music, desc } = result.result;
+            const { avatar } = result.result.author;
 
-        // Get 
-        const uID = await context.interaction.guild?.members.fetch(target.id);
-        const uIcon = target.displayAvatarURL();
+            // Buttons
+            const videos = new ButtonBuilder()
+                .setLabel('Download Video')
+                .setURL(`${video}`)
+                .setStyle(ButtonStyle.Link);
 
-        //Embed
-        const embed = new EmbedBuilder()
-            .setTitle(target.displayName)
-            .setThumbnail(uIcon)
-            .addFields({ name: "Member", value: `${target}`, inline: false })
-            .addFields({ name: "Roles", value: `${uID?.roles.cache.map(r => r).join(' \n')}`, inline: false })
-            .addFields({ name: "Joined Server", value: `${uID?.joinedAt}`, inline: false })
-            .addFields({ name: "Joined Discord", value: `${context.interaction.options.getUser("target")?.createdAt}`, inline: false })
-            .addFields({ name: "User ID", value: `${target.id}`, inline: false })
-            .setFooter({ text: `Respond within ${ping}ms` })
-            .setTimestamp()
+            const audios = new ButtonBuilder()
+                .setLabel('Download Music')
+                .setURL(`${music}`)
+                .setStyle(ButtonStyle.Link);
 
-        await context.sendInteraction({ embeds: [embed] });
+            const row = new ActionRowBuilder<ButtonBuilder>()
+                .addComponents(videos, audios)
+
+            //Embed
+            const embed = new EmbedBuilder()
+                .setTitle(desc)
+                .setThumbnail(avatar)
+                .setFooter({ text: `Respond within ${ping}ms` })
+                .setTimestamp()
+
+            await context.sendInteraction({ embeds: [embed], components: [row] });
+        } else {
+            // Handle case where result is not as expected
+            await context.sendInteraction({ content: "Failed to retrieve TikTok video information.", ephemeral: true });
+        }
     }
 }
